1de42a8705a6309eeef70a03972f3584a4696552
saikumar <saisunny2330@gmail.com>
2023-06-26 15:05:05 +0000

    all test cases fail

---
+++ src/components/SuggestionItem/index.js
@@ -0,0 +1,28 @@
+import './index.css'
+const Suggestions = props => {
+  const {suggestionDetails, updateSearchInput} = props
+  const {suggestion} = suggestionDetails
+
+  const onClickSuggestion = () => {
+    uodateSearchInput(suggestion)
+  }
+
+  return (
+    <li className="suggestion-item">
+      <p className="suggestion-text">{suggestion}</p>
+      <button
+        type="button"
+        className="arrow-button"
+        onClick={onClickSuggestion}
+      >
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"
+          alt="arrow"
+          className="arrow"
+        />
+      </button>
+    </li>
+  )
+}
+
+export default SuggestionItem
